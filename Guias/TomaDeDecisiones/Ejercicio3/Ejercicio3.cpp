847657498150fa4ebe88a57acb04e8e5092d86fe
AP_MejiaMendez <00098225@uca.edu.sv>
2025-05-11 19:21:06 -0600

    Ejercicio 3

---
+++ Guias/TomaDeDecisiones/Ejercicio3/Ejercicio3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Num = 0;
+    bool By5 = false, By3 = false;
+
+    cout << "Welcome to the program that tells you if a number is divisible by 5 and 3" << endl;
+    cout << "Enter the number: ";
+    cin >> Num;
+
+    By5 = (Num % 5 == 0);
+    By3 = (Num % 3 == 0);
+
+    if (By5 && By3)
+    {
+        cout << "The number is divisible by both 5 and 3";
+    }
+    else if (By5)
+    {
+        cout << "The number is divisible by 5 but not by 3";
+    }
+    else if (By3)
+    {
+        cout << "The number is divisible by 3 but not by 5";
+    }
+    else
+    {
+        cout << "The number is NOT divisible by 5 or by 3";
+    }
+
+    return 0;
+}
