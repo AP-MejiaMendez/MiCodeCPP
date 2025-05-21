af773a97d71279738bf08367f809723ee249aded
AP_MejiaMendez <00098225@uca.edu.sv>
2025-05-21 12:34:11 -0600

    Update

--- Clases/Lunes_19-5/Ejercicio10_Fibonacci.cpp
+++ Clases/Lunes_19-5/Ejercicio10_Fibonacci.cpp
@@ -10,12 +10,10 @@ int main()
 {
     // Declaracion del limite de numeros
     int limiteFibonacci = 2;
-
     // Mensaje de contexto al usuario
     cout << "Bienvenido al programa que te muestra la serie de fibonacci!" << endl;
     cout << "Ingrese cuantos terminos de Fibonacci quieres ver: ";
     cin >> limiteFibonacci;
-
     // Condicional para que el usuario no pida 0 o menos terminos
     if (limiteFibonacci <= 0)
     {
@@ -25,22 +23,14 @@ int main()
     {
         // Declaración de los arrays a utilizar
         int resultadoFibonacci[limiteFibonacci] = {0, 1};
-
+        cout << endl;
+        cout << "Resultado: " << endl;
         // Bucle que itera en las posiciones de los resultados fibonacci para guardar la posicion anterior mas la posicion actual
         for (int i = 1; i <= limiteFibonacci; i++)
         {
             resultadoFibonacci[i + 1] = resultadoFibonacci[i - 1] + resultadoFibonacci[i];
-        }
-
-        cout << endl;
-        cout << "Resultado: " << endl;
-
-        //Bucle que itera en los resultados del arreglo resultadosFibonacci para mostrarlos
-        for (int i = 1; i <= limiteFibonacci; i++)
-        {
             cout << i << ") " << resultadoFibonacci[i - 1] << " + " << resultadoFibonacci[i] << " = " << resultadoFibonacci[i + 1] << endl;
         }
     }
-
     return 0;
 }
