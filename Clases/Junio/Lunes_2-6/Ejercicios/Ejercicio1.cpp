701a539342f8611cec02bbed9ddcf23cc6ae4201
AP_MejiaMendez <00098225@uca.edu.sv>
2025-06-02 11:47:50 -0600

    Update

--- Clases/Junio/Lunes_2-6/Ejercicios/Ejercicio1.cpp
+++ Clases/Junio/Lunes_2-6/Ejercicios/Ejercicio1.cpp
@@ -1,9 +1,43 @@
 /*
-*/
+ */
 #include <iostream>
 using namespace std;
 
+int mayorDeTres(int a, int b, int c)
+{
+
+    if (a > b && a > c)
+    {
+        return a;
+    }
+    else if (b > c)
+    {
+        return b;
+    }
+    else
+    {
+        return c;
+    }
+}
+
+bool esPar(int n)
+{
+
+    return n % 2 == 0;
+}
+
 int main()
 {
+    int a = 3, b = 9, c = 5;
+    int mayor3 = mayorDeTres(a, b, c);
+
+    if (esPar(mayor3))
+    {
+        cout << mayor3 << " Es par" << endl;
+    }
+    else
+    {
+        cout << mayor3 << " Es impar" << endl;
+    }
     return 0;
 }
